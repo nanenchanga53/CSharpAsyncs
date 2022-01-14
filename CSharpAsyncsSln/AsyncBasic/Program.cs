9928aa51011f3437c5ea1a07a6c520f38c493f82
nanenchanga53 <nanenchanga53@gmail.com>
2022-01-14 22:00:02 +0900

    많이쓰는거 종료 다음부터는 어려운거 혹은 잘쓰기위한 정리소스를 제작예정

--- CSharpAsyncsSln/AsyncBasic/Program.cs
+++ CSharpAsyncsSln/AsyncBasic/Program.cs
@@ -3,8 +3,9 @@ Console.WriteLine("C# Async Basic");
 
 await BasicAsync(); // Task 가 기다리길 기다리도록 await(없다면 Task가 끝나는 걸 기다리지 않음) await를 사용안하려면 메서드를 async를 넣지않고 만들자
 
-await BasicExeptionAsync();
+await BasicExeptionAsync(); // 에러가 나는 곳을 확인하자
 
+BasicRotateMatrices(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 });
 
 
 /// <summary>
@@ -46,8 +47,31 @@ async Task BasicExeptionAsync()
 
 }
 
-async Task PossibleExeptionAsync() // 역시 async일때 await에 반환이 가능하고 아니면 첫 호출한 곳으로 바로 에로가 반환된다.
+async Task PossibleExeptionAsync() // 역시 async일때 await에 반환이 가능하고 아니면 첫 호출한 곳으로 바로 에러가 반환된다.
 {
         await Task.Delay(TimeSpan.FromSeconds(1));
         throw new NotImplementedException();
+}
+
+void BasicRotateMatrices(IEnumerable<int> values)
+{
+        Parallel.ForEach(values, x => Console.Write(x)); // 병렬로 실행되며 2번째에는 실행할 것을 넣자
+        Console.WriteLine();
+        var result = values.AsParallel().Select(x => x); // linq로 선택된것을 병렬로 가져온다 순서가 중요하면 보통의 foreach를 사용하자
+        foreach(var v in result)
+                Console.Write(v.ToString());
+        Console.WriteLine();
+
+        try
+        {
+                Parallel.Invoke(() => { throw new Exception(); }, () => { throw new Exception(); });
+        }
+        catch (AggregateException ex)// Parallel.Invoke는 어떤 순서에서 에러가 났는지 확인하기 힘들지만 이 오류처리 코드를 통해 쉽게 만들 수 있다.
+        { 
+                ex.Handle(exception =>
+                {
+                        Console.WriteLine(exception);
+                        return true;
+                });
+        }
 }
